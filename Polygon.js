02a48dd17f2d98b37aa64126811f7b766faa1de3
Chanakyapandya <72978459+Chanakyapandya@users.noreply.github.com>
2020-11-21 21:52:22 +0530

    Add files via upload

---
+++ Polygon.js
@@ -0,0 +1,27 @@
+class Polygon {
+    constructor(x,y,r) {
+      var options = {
+          isStatic: false
+      }
+     this.body = Bodies.circle(x,y,r);
+     // this.width = width;
+      //this.height = height;
+      this.x = x;
+      this.y = y;
+      this.r = r;
+
+   //  this.image2 = loadImage("polygon.png")
+
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+
+      push();
+      imageMode(CENTER)
+      translate(pos.x,pos.y)
+
+      image(this.image,0,0, this.width, this.height);
+      pop()
+    }
+  }
